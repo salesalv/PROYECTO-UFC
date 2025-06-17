44f7bc9c51325c4e183ec347fa5c9cd6e91c9003
Alvaro Sales <salesalvaro794@gmail.com>
2025-06-17 15:25:20 -0300

    c

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import cors from 'cors';
+
 
 const supabaseUrl = 'https://gqylknstxjwlybyvvcvx.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdxeWxrbnN0eGp3bHlieXZ2Y3Z4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwOTMyNzIsImV4cCI6MjA2NTY2OTI3Mn0.fdOlalxcPaKzpjaUnFowsY3gz6f0LYmCdE--aJ76zc8';
