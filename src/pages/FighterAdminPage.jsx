cb593bbd588743e77f13d05131642523002088a8
Alvaro Sales <salesalvaro794@gmail.com>
2025-09-27 18:31:27 -0300

    smash

--- src/pages/FighterAdminPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import FighterAdminPanel from '@/components/admin/FighterAdminPanel';
-
-const FighterAdminPage = () => {
-  return <FighterAdminPanel />;
-};
-
-export default FighterAdminPage;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -19,7 +19,6 @@ import CategoryPage from '../pages/CategoryPage';
 import UFCNamajunasBarberCardPage from '../pages/UFCNamajunasBarberCardPage';
 import UFC304CardPage from '../pages/UFC304CardPage';
 import UFCSandhagenNurmagomedovCardPage from '../pages/UFCSandhagenNurmagomedovCardPage';
-import FighterAdminPage from '@/pages/FighterAdminPage';
 
 const AppRoutes = () => {
   return (
@@ -43,7 +42,6 @@ const AppRoutes = () => {
       <Route path="/clips" element={<SavedClipsPage />} />
       <Route path="/thread/:id" element={<ThreadPage />} />
       <Route path="/category/:id" element={<CategoryPage />} />
-      <Route path="/admin/fighters" element={<FighterAdminPage />} />
     </Routes>
   );
 };
